bebf623ab54a5f9cacb8e46f80961c9978df03fc
Realiza <realiza@Realiza-16.local>
2017-06-23 15:33:57 -0300

    criação da pasta do lucas

---
+++ works/lucas_curi/2017_06_21/la.js
@@ -0,0 +1,27 @@
+﻿var pessoas = 1;
+
+$("#salvar").on("click", function() {
+	var nome = $("[name = nome]").val();
+	var idade = $("[name = idade]").val();
+	
+
+	if (nome == "") {
+		alert("Por favor, forneça todas as informações");
+		$("[name = nome]").addClass("erro");
+		return false;
+	} else if (idade == "") {
+		alert("Por favor, forneça todas as informações");
+		$("[name = idade]").addClass("erro");
+		return false;
+	}
+
+	else {
+		$("[name = nome]").removeClass("erro");
+		$("[name = idade]").removeClass("erro");
+		$('.tabela > thead').append("<tr> <td> "+ nome +" </td> <td>"+ idade +" </td> </tr>");
+		$("#pessoas").text(pessoas++);
+      	$("#nome").focus();
+	}
+	$(this).next('.tabela').fadeIn(200);
+	
+});
